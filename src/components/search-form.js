2321a31c36d5990e42b041a2de36a3599c73c435
Roderic Linguri <linguri@digices.com>
2018-06-06 06:47:34 -0600

    Change button color to match React theme

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -6,7 +6,7 @@ export class SearchForm extends Component {
     return (
       <form className="form-inline my-2 my-lg-0">
         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button className="btn btn-outline-react my-2 my-sm-0" type="submit">Search</button>
       </form>
     );
   } // ./render
